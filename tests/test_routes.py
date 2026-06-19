c37fdc8bfdd973a0cbb7741cb60fed78b399d527
ankitmahato54 <ankitmahato54@gmail.com>
2026-06-19 06:42:11 -0400

    Implemented READ product endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -163,9 +163,30 @@ class TestProductRoutes(TestCase):
         response = self.client.post(BASE_URL, data={}, content_type="plain/text")
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    #
-    # ADD YOUR TEST CASES HERE
-    #
+    def test_get_product(self):
+        """It should Read a Product"""
+        test_product = self._create_products(1)[0]
+
+        response = self.client.get(
+            f"{BASE_URL}/{test_product.id}"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.get_json()
+
+        self.assertEqual(data["id"], test_product.id)
+        self.assertEqual(data["name"], test_product.name)
+        self.assertEqual(data["description"], test_product.description)
+        self.assertEqual(
+            Decimal(data["price"]), test_product.price
+        )
+        self.assertEqual(
+            data["available"], test_product.available
+        )
+        self.assertEqual(
+            data["category"], test_product.category.name
+        )
 
     ######################################################################
     # Utility functions
