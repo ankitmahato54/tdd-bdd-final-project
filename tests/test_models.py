bd5c183b4f3994d46a1f09883ea1de4bbccc6c1e
ankitmahato54 <ankitmahato54@gmail.com>
2026-06-19 05:08:56 -0400

    Added find by name

--- tests/test_models.py
+++ tests/test_models.py
@@ -159,3 +159,21 @@ class TestProductModel(unittest.TestCase):
             product.create()
 
         self.assertEqual(len(Product.all()), 5)
+
+    def test_find_by_name(self):
+        """It should Find Products by Name"""
+        products = ProductFactory.create_batch(5)
+
+        for product in products:
+            product.id = None
+            product.create()
+
+        name = products[0].name
+        count = len([p for p in products if p.name == name])
+
+        found = Product.find_by_name(name)
+
+        self.assertEqual(found.count(), count)
+
+        for product in found:
+            self.assertEqual(product.name, name)
